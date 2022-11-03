7a0bdf9fd9870a25ce8028b3a118b95a9bd2b14c
DeividasRo <slenderiss@gmail.com>
2022-11-03 19:03:21 +0200

    finished

--- src/calculator.js
+++ src/calculator.js
@@ -17,7 +17,7 @@ function resetInput() {
     previousOperator = null;
 }
 
-function clearLastElement() {
+function clearLastInputElement() {
     let val = input.value;
     val = val.substring(0, val.length - 1);
     input.value = val;
@@ -29,43 +29,89 @@ function display(value) {
 
 
 function numFunc(value) {
+    if (input.value == "Infinity") clearInput();
     display(value);
+
 }
 
-function opFunc(value) {
-    twoOperandBehaviour(value);
+function opFunc1(operator) {
+    if (input.value == "Infinity") clearInput();
+    updateOperands();
+    if (firstOperand == null || firstOperand == "") return;
+
+    calculate();
+    if (!isNum(input.value.slice(-1)[0])) { clearLastInputElement(); }
+    updateOperands();
+
+    var result;
+    if (operator == '√') {
+        result = Math.sqrt(parseFloat(firstOperand));
+    }
+    else if (operator == '²') {
+        result = Math.pow(parseFloat(firstOperand), 2);
+    }
+    else if (operator == '³') {
+        result = Math.pow(parseFloat(firstOperand), 3);
+    }
+
+    clearInput();
+    display(Number(result.toFixed(3)));
 }
 
-function twoOperandBehaviour(operator) {
-    if (!isNum(input.value.slice(-1)[0])) return;
 
-    firstOperand = input.value.split(previousOperator)[0];
-    secondOperand = input.value.split(previousOperator)[1];
+function opFunc2(operator) {
+    if (input.value == "Infinity") clearInput();
+    updateOperands();
+    if (firstOperand == null || firstOperand == "") return;
 
-    console.log(firstOperand, secondOperand);
+    if (!isNum(input.value.slice(-1)[0])) {
+        clearLastInputElement();
+        display(operator);
+        previousOperator = operator;
+        return;
+    }
+
+    updateOperands();
 
     display(operator);
     if (secondOperand != null) {
+        calculate();
+        if (input.value != "Infinity")
+            display(operator);
+    }
+
+    previousOperator = operator;
+}
+
+function eqFunc() {
+    if (input.value == "Infinity") clearInput();
+    updateOperands();
+    calculate();
+}
+
+function updateOperands() {
+    firstOperand = input.value.split(previousOperator)[0];
+    secondOperand = input.value.split(previousOperator)[1];
+}
+
+function calculate() {
+    console.log(firstOperand, secondOperand, previousOperator);
+    if (secondOperand != null && secondOperand != "") {
         clearInput();
         var result;
         if (previousOperator == '+')
             result = parseFloat(firstOperand) + parseFloat(secondOperand);
-        if (previousOperator == '-')
+        else if (previousOperator == '-')
             result = parseFloat(firstOperand) - parseFloat(secondOperand);
-        if (previousOperator == "*")
+        else if (previousOperator == "*")
             result = parseFloat(firstOperand) * parseFloat(secondOperand);
-        if (previousOperator == "/")
+        else if (previousOperator == "/")
             result = parseFloat(firstOperand) / parseFloat(secondOperand);
-        if (previousOperator == "%")
+        else if (previousOperator == "%")
             result = parseFloat(firstOperand) % parseFloat(secondOperand);
 
-        display(Number(result.toFixed(3)) + operator);
+        display(Number(result.toFixed(3)));
         previousOperator = null;
         secondOperand = null;
     }
-
-    previousOperator = operator;
-
-
-    // change color of operator in action
 }
